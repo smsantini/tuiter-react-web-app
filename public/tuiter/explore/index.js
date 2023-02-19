261012966bedd078e619472d522e9f0da0807033
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-02-19 18:24:14 -0500

    test again

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,30 +1,28 @@
-// import navigationSidebar from "./NavigationSidebar";
-// import whoToFollowList from "./WhoToFollowList";
-// import exploreComponent from "./ExploreScreen";
-//
-// function exploreComponentFull() {
-//     $('#wd-explore').append(`
-//         <h2>Explore</h2>
-//         <div class="row mt-2">
-//             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-// <!--                <h3>NavigationSidebar</h3>-->
-//                 ${navigationSidebar()}
-//             </div>
-//             <div class="col-10 col-lg-7 col-xl-6">
-// <!--                <h3>ExploreComponent</h3>-->
-//                 ${exploreComponent()}
-//             </div>
-//             <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-// <!--                <h3>WhoToFollowList </h3>-->
-//                 ${whoToFollowList()}
-//             </div>
-//         </div>
-//    `);
-// }
-//
-// exploreComponentFull();
+import navigationSidebar from "./NavigationSidebar";
+import whoToFollowList from "./WhoToFollowList";
+import exploreComponent from "./ExploreScreen";
 
-const init = () => {
-    console.log('Hello world from jQuery');
+function exploreComponentFull() {
+    $('#wd-explore').append(`
+        <h2>Explore</h2>
+        <div class="row mt-2">
+            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                <h3>NavigationSidebar</h3>
+            </div>
+            <div class="col-10 col-lg-7 col-xl-6">
+                <h3>ExploreComponent</h3>
+            </div>
+            <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                <h3>WhoToFollowList </h3>
+            </div>
+        </div>
+   `);
 }
-$(init);
+
+exploreComponentFull();
+
+/*
+${navigationSidebar()}
+${exploreComponent()}
+${whoToFollowList()}
+ */
