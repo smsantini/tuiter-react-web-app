796ca4cff7b7b80f297044418af47ad6960be28d
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-02-19 18:28:18 -0500

    test

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,9 +1,12 @@
+import NavigationSidebar from "NavigationSidebar";
+
 function exploreComponentFull() {
     $('#wd-explore').append(`
         <h2>Explore</h2>
         <div class="row mt-2">
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 <h3>NavigationSidebar</h3>
+                ${NavigationSidebar()}
             </div>
             <div class="col-10 col-lg-7 col-xl-6">
                 <h3>ExploreComponent</h3>
@@ -18,7 +21,7 @@ function exploreComponentFull() {
 exploreComponentFull();
 
 /*
-import navigationSidebar from "./NavigationSidebar";
+
 import whoToFollowList from "./WhoToFollowList";
 import exploreComponent from "./ExploreScreen";
 ${navigationSidebar()}
