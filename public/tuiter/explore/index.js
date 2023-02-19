aa7eccf1d55f52a59e10a191bf218f260e37f444
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-02-19 18:12:51 -0500

    more potential fixes

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,22 +1,22 @@
-import NavigationSidebar from "./NavigationSidebar";
-import WhoToFollowList from "./WhoToFollowList";
-import ExploreComponent from "./ExploreScreen";
+import navigationSidebar from "./NavigationSidebar";
+import whoToFollowList from "./WhoToFollowList";
+import exploreComponent from "./ExploreScreen";
 
-function exploreComponent()  {
+function exploreComponent() {
     $('#wd-explore').append(`
         <h2>Explore</h2>
         <div class="row mt-2">
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
 <!--                <h3>NavigationSidebar</h3>-->
-                ${NavigationSidebar()}
+                ${navigationSidebar()}
             </div>
             <div class="col-10 col-lg-7 col-xl-6">
 <!--                <h3>ExploreComponent</h3>-->
-                ${ExploreComponent()}
+                ${exploreComponent()}
             </div>
             <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
 <!--                <h3>WhoToFollowList </h3>-->
-                ${WhoToFollowList()}
+                ${whoToFollowList()}
             </div>
         </div>
    `);
